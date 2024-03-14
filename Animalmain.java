781a4425dd95dd42a7a9b0c07c5bd12d0d9c0e25
Ananya Singh <99748964+2456373736@users.noreply.github.com>
2024-03-14 10:31:31 +0530

    Add files via upload

---
+++ Animalmain.java
@@ -0,0 +1,12 @@
+public class Animalmain
+{
+public static void main (String args [])
+{
+     	Animal1 animal= new Animal1();
+// accessing the public variable 
+        animal.legCount =4;
+        animal.display();
+
+        
+}
+}
